a60fa21c248d06a88bf964a8edebb7654b756c9c
Min Kyung CHOI <haleychoi0918@gmail.com>
2020-08-31 14:35:22 +1000

    create Recipe component and functions to fetch the data from api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
+import Recipe from './Recipe'
 import './App.css';
 
 const App = () =>{
@@ -6,11 +7,41 @@ const App = () =>{
   const APP_ID = 'a50e3aac'
   const APP_KEY = process.env.REACT_APP_SECRET_KEY
 
-  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('chicken')
+
+  useEffect( ()=>{
+    getRecipes()
+  },[query])
+
+  const getRecipes = async() =>{
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json()
+    setRecipes(data.hits)
+    //console.log(data.hits)
+  }
+
+  const updateSearch = e =>{
+    setSearch(e.target.value)
+    //console.log(search)
+  }
+
+  const getSearch = e =>{
+    e.preventDefault()
+    setQuery(search)
+    setSearch('')
+  }
 
   return(
     <div className="App">
-      <h1>Hello React</h1>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      {recipes.map((recipe, index)=>(
+        <Recipe key={index} title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image}/>
+      ))}
     </div>
   )
 }
